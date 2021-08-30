6988c8935c943a59246ccec5b77303f2c24678f8
zhixun <327354573@qq.com>
2021-08-30 21:46:18 +0800

    20210830

--- app/src/main/java/com/zhixun/javacode/other/Test3.java
+++ app/src/main/java/com/zhixun/javacode/other/Test3.java
@@ -1,30 +1,40 @@
 package com.zhixun.javacode.other;
 
+import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class Test3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
-		ses.submit(new Runnable() {
-			@Override
-			public void run() {
-				for (int i = 0; i < Integer.MAX_VALUE; i++) {
-					// Log.w("Test1", "i = " + i);
-					System.out.println("i = " + i);
-					if (i == 50) {
-						System.out.println("终止");
-						ses.shutdownNow();
-					}
-					if (i == 60) {
-						System.out.println("break");
-						break;
-					}
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ses.submit(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                    // Log.w("Test1", "i = " + i);
+                    System.out.println("i = " + i);
+                    if (i == 50) {
+                        System.out.println("终止");
+                        ses.shutdownNow();
+                    }
+                    if (i == 60) {
+                        System.out.println("break");
+                        break;
+                    }
+                }
+            }
+        });
+        Hslx sf=new Hslx();
+        sf.sdsa();
+    }
 
+    static class Hslx {
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        public void sdsa() {
+            hashMap.put("12", "23");
+        }
+    }
 }
