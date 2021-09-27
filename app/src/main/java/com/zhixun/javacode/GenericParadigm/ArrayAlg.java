6af46d768d96c64c8e5a911e304e09d9339ac6e4
zhixun <327354573@qq.com>
2021-09-27 23:02:15 +0800

    泛型2

--- app/src/main/java/com/zhixun/javacode/GenericParadigm/ArrayAlg.java
+++ app/src/main/java/com/zhixun/javacode/GenericParadigm/ArrayAlg.java
@@ -5,8 +5,23 @@ package com.zhixun.javacode.GenericParadigm;
  * 1.泛型方法可以定义在普通类中也可以定义在泛型类中
  * 2.泛型标识符放在修饰符后面，返回类型的前面
  */
-public class ArrayAlg {
-    public <T> T getMiddle(T... arg) {
+public class ArrayAlg<E> {
+    //1. 定义泛型方法  修饰符 <泛型列表> 返回类型 方法名
+    public < E> E getMiddle1(E... arg) {
+        return arg[arg.length / 2];
+    }   //2. 泛型方法泛型独立于泛型类泛型（如果泛型类泛型标识是String，泛型方法可以使用Integer）
+
+    //3. 泛型类的成员方法不能使静态方法
+    public E getMiddle(E... arg) {
         return arg[arg.length / 2];
     }
+
+    ///4. 泛型方法可以使用静态修饰
+    public static <T, E> T getMiddle2(T... arg) {
+        return arg[arg.length / 2];
+    }
+
+
 }
+
+
--- app/src/main/java/com/zhixun/javacode/GenericParadigm/Execute.java
+++ app/src/main/java/com/zhixun/javacode/GenericParadigm/Execute.java
@@ -3,10 +3,10 @@ package com.zhixun.javacode.GenericParadigm;
 public class Execute {
     public static void main(String[] arg) {
         System.out.println("-----泛型方法-----");
-        ArrayAlg alg = new ArrayAlg();
+        ArrayAlg<Integer> alg = new ArrayAlg<Integer>();
         //1.调用泛型方法
-        alg.<String>getMiddle("", "");//调用时在方法名前的尖括号放入具体类型
-        alg.getMiddle("", "");//大多数情况不用放，编译器能够推断类型
+        alg.<String>getMiddle1("", "");//调用时在方法名前的尖括号确定泛型类型
+        alg.getMiddle1("", "");//大多数情况不用放，编译器能够推断类型
 //        System.out.println( double d = alg.getMiddle(1.1f, 2.5f, 1));
     }
 }
